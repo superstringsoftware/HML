d41235d127eb998d516c9e08bace760b13d4e12d
J Ho XRay <jho.xray@gmail.com>
2018-04-06 20:55:15 +0200

    moved accelerate to primary version

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,99 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
+-- |
+-- Module      : Main
+-- Copyright   : [2018] J X-Ray Ho
+-- License     : BSD3
+--
+-- Maintainer  : J X-Ray Ho <jho.xray@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
 
 module Main where
 
 import Lib
-import Criterion.Main
-import Data.Vector.Unboxed as U
+import ShallowNN
+import AccelerateCore.Base
 
-import System.Random.Mersenne
-import qualified Data.Vector.Random.Mersenne as G
+import Text.Printf
+import Prelude                                                      as P
 
-randomVec1 :: Int -> IO (U.Vector Double)
-randomVec1 n = do
-  g <- newMTGen Nothing
-  a <- G.random g n :: IO (U.Vector Double)
-  return a
+import Data.Array.Accelerate                                        as A
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+import Data.Array.Accelerate.LLVM.Native                            as CPU
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+import Data.Array.Accelerate.LLVM.PTX                               as PTX
+#endif
 
-setupEnv = do
-  gen <- newMTGen Nothing
-  small <- G.random gen 10000000 :: IO (U.Vector Double)
-  big <- G.random gen 10 :: IO (U.Vector Double)
-  return (small, big)
+import Data.Array.Accelerate.System.Random.MWC
 
-main =
-  defaultMain [
-     -- notice the lazy pattern match here!
-     env setupEnv $ \ ~(small,big) -> bgroup "main" [
-     bgroup "small" [
-       bench "foldl" $ nf (dotV1 small) small
-     , bench "foldl'" $ nf (dotV2 small) small
-     -- , bench "foldr" $ nf (dotV3 small) small
-     , bench "foldr'" $ nf (dotV4 small) small
-     , bench "sum" $ nf (dotV5 small) small
-     , bench "foldl no zip" $ nf (dotV6 small) small
-     , bench "foldl' no zip" $ nf (dotV7 small) small
-     -- , bench "foldM' no zip" $ whnf (dotV8 small) small
-     ]
-  ] ]
+type Matrix = Array DIM2
+
+main :: IO ()
+main = do
+  let
+      xs :: Vector Double
+      xs = fromList (Z:.10) [0..]
+
+      ys :: Vector Double
+      ys = fromList (Z:.10) [1,3..]
+
+      zs0 :: IO  (Vector Double) 
+      zs0 = randomArray (uniformR (0.0 :: Double,1.0 :: Double)) (Z:.10)
+
+  zs <- zs0
+
+  matr <- numpyRand 2 2
+
+  printf "input data:\n"
+  printf "xs = %s\n" (show xs)
+  printf "ys = %s\n\n" (show ys)
+
+  printf "zs = %s\n\n" (show zs)
+
+  printf "mx = %s\n\n" (show matr)
+  printf "testNet = %s\n\n" (show testNet0)
+  
+  printf "the function to execute:\n"
+  printf "%s\n\n" (show dotp)
+
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+  printf "result with CPU backend: dotp xs ys = %s\n" (show (CPU.runN dotp xs ys))
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+  printf "result with PTX backend: dotp xs ys = %s\n" (show (PTX.runN dotp xs ys))
+#endif
+  
+  printf "\n\n"
+  testComputeZ
+
+
+-- ***************** BASIC TESTS **************************
+testComputeZ :: IO ()
+testComputeZ = do
+    let
+        a :: Vector NNElement
+        a = fromList (Z:.2) [2,3]
+
+        b :: Vector NNElement
+        b = fromList (Z:.2) [5,3]
+  
+        w :: Matrix NNElement
+        w = fromList (Z:.2:.2) [1,2,4,3]
+
+    printf "input data:\n"
+    printf "a = %s\n" (show a)
+    printf "b = %s\n\n" (show b)
+    
+    printf "w = %s\n\n" (show w)
+
+    printf "the function to execute:\n"
+    printf "%s\n\n" (show computeZ)
+    
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+    printf "result with CPU backend: computeZ w a b = %s\n" (show (CPU.runN computeZ w a b))
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+    printf "result with PTX backend: computeZ w a b = %s\n" (show (PTX.runN computeZ w a b))
+#endif
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,49 +1,23 @@
-module Lib
-    where
+{-# LANGUAGE RebindableSyntax #-}
+-- |
+-- Module      : Lib
+-- Copyright   : [2018] J X-Ray Ho
+-- License     : BSD3
+--
+-- Maintainer  : J X-Ray Ho <jho.xray@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
 
-import System.Random
-import Data.Vector.Unboxed as U
-import System.Random.Mersenne
-import qualified Data.Vector.Random.Mersenne as G
-import GHC.Prim
+module Lib (
 
-randomVec :: Int -> IO (U.Vector Double)
-randomVec n = do
-  g <- newMTGen Nothing
-  a <- G.random g n :: IO (U.Vector Double)
-  return a
+  dotp
 
--- different implementations of dot product of 2 vectors
-dotV1 :: U.Vector Double -> U.Vector Double -> Double
-dotV1 v1 v2 = U.foldl (+) 0 $ U.zipWith (*) v1 v2
+) where
 
-dotV2 :: U.Vector Double -> U.Vector Double -> Double
-dotV2 v1 v2 = U.foldl' (+) 0 $ U.zipWith (*) v1 v2
+import Data.Array.Accelerate
 
-dotV3 :: U.Vector Double -> U.Vector Double -> Double
-dotV3 v1 v2 = U.foldr (+) 0 $ U.zipWith (*) v1 v2
-
-dotV4 :: U.Vector Double -> U.Vector Double -> Double
-dotV4 v1 v2 = U.foldr' (+) 0 $ U.zipWith (*) v1 v2
-
-dotV5 :: U.Vector Double -> U.Vector Double -> Double
-dotV5 v1 v2 = U.sum $ U.zipWith (*) v1 v2
-
-dotV6 :: U.Vector Double -> U.Vector Double -> Double
-dotV6 v1 v2 = U.ifoldl func 0 v2
-  where func acc i el = acc + el *  (v1 U.! i)
-
-dotV7 :: U.Vector Double -> U.Vector Double -> Double
-dotV7 v1 v2 = U.ifoldl' func 0 v2
-  where func acc i el = acc + el * (v1 U.! i)
-
-dotV8 :: U.Vector Double -> U.Vector Double -> IO Double
-dotV8 v1 v2 = U.ifoldM' func 0 v2
-  where func acc i el = pure $ acc + el * (v1 U.! i)
-
-dotV9 :: U.Vector Double -> U.Vector Double -> IO Double
-dotV9 v1 v2 = U.ifoldM' func 0 v2
-  where func acc i el = pure $ acc + el * (v1 U.! i)
-
-sigmoid :: Floating a => a -> a
-sigmoid x = 1 / (1 + exp (-x))
+-- | A simple vector inner product
+--
+dotp :: Acc (Vector Double) -> Acc (Vector Double) -> Acc (Scalar Double)
+dotp xs ys = fold (+) 0 ( zipWith (*) xs ys)
